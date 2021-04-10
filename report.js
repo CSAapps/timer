79d16a3ed50c7ddfbbf93ddc2f188084fdb6fe0a
KCSAbeywickrama <abeywickramakcs.19@uom.lk>
2021-04-10 19:47:06 +0530

    add report table

--- report.js
+++ report.js
@@ -2,8 +2,14 @@ function e(id) {
     return document.getElementById(id);
 }
 
+var tbl = e('tbl');
+
+function formatTime(time) {
+    return `${parseInt(time/60)}:${time%60}`
+}
 var report = JSON.parse(localStorage.report);
 
 report.forEach(e => {
-    document.body.innerHTML += `${e.name}:${e.time}<br>`
+    var row = `<tr><td>${e.name}</td><td>${formatTime(e.time)}</td></tr>`
+    tbl.innerHTML += row;
 });
