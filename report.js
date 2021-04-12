c9b0ce89687e4dae88e7a2ddbdb095178b56bcd8
KCSAbeywickrama <abeywickramakcs.19@uom.lk>
2021-04-12 18:11:41 +0530

    report styling & repor clear button

--- report.js
+++ report.js
@@ -3,13 +3,26 @@ function e(id) {
 }
 
 var tbl = e('tbl');
+var btnClr = e('btn-clr');
+var tblDiv = e('tbl-div');
 
 function formatTime(time) {
-    return `${parseInt(time/60)}:${time%60}`
+    return `${parseInt(time/60)} : ${String(time%60).padStart(2, '0')}`
 }
 var report = JSON.parse(localStorage.report);
 
-report.forEach(e => {
-    var row = `<tr><td>${e.name}</td><td>${formatTime(e.time)}</td></tr>`
-    tbl.innerHTML += row;
-});
+for (var i = 0; i < 1; i++) {
+    if (report.length) {
+        report.forEach(e => {
+            var row = `<tr><td>${e.name}</td><td>${formatTime(e.time)}</td></tr>`
+            tbl.innerHTML += row;
+        });
+    } else {
+        tblDiv.innerHTML = "No saved reports"
+    }
+}
+
+btnClr.onclick = function() {
+    localStorage.report = JSON.stringify([]);
+    tblDiv.innerHTML = "No saved reports"
+}
